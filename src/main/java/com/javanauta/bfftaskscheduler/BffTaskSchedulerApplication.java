a6bca5cd526bd70a3eaa3f731e6e33af704468d1
Marina <marinabrito.ma@gmail.com>
2025-02-15 09:54:25 +0000

    initial commit

---
+++ src/main/java/com/javanauta/bfftaskscheduler/BffTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.bfftaskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffTaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffTaskSchedulerApplication.class, args);
+	}
+
+}
