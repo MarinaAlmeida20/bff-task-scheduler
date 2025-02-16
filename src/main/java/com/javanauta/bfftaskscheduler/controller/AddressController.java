b52a0303239709add7a599704d80d4f86087996a
Marina <marinabrito.ma@gmail.com>
2025-02-16 15:13:04 +0000

    added PhoneNumberController, PhoneNumberDTO, AddressController and AddressDTO

---
+++ src/main/java/com/javanauta/bfftaskscheduler/controller/AddressController.java
@@ -0,0 +1,41 @@
+package com.javanauta.bfftaskscheduler.controller;
+
+
+import com.javanauta.bfftaskscheduler.business.UserService;
+import com.javanauta.bfftaskscheduler.business.dto.AddressDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@Tag(name = "User", description = "User registration and login")
+public class AddressController {
+
+    private final UserService userService;
+
+    @PostMapping("/address")
+    @Operation(summary = "Save user address", description = "Save user address")
+    @ApiResponse(responseCode = "200", description = "Address success saved")
+    @ApiResponse(responseCode = "404", description = "User not found")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    public ResponseEntity<AddressDTO> addToAddress(@RequestBody AddressDTO addressDTO,
+                                                   @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(userService.addToAddress(token, addressDTO));
+    }
+
+    @PutMapping("/address")
+    @Operation(summary = "User address updated", description = "User address updated")
+    @ApiResponse(responseCode = "200", description = "Address success updated")
+    @ApiResponse(responseCode = "404", description = "User not found")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    public ResponseEntity<AddressDTO> updateAddress(@RequestBody AddressDTO addressDTO,
+                                                    @RequestParam("id") Long id,
+                                                    @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(userService.updateAddress(id, addressDTO, token));
+    }
+}
---
+++ src/main/java/com/javanauta/bfftaskscheduler/controller/PhoneNumberController.java
@@ -0,0 +1,40 @@
+package com.javanauta.bfftaskscheduler.controller;
+
+import com.javanauta.bfftaskscheduler.business.UserService;
+import com.javanauta.bfftaskscheduler.business.dto.PhoneNumberDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@Tag(name = "User", description = "User registration and login")
+public class PhoneNumberController {
+
+    private final UserService userService;
+
+    @PostMapping("/phoneNumber")
+    @Operation(summary = "Save user phone number", description = "Save user phone number")
+    @ApiResponse(responseCode = "200", description = "Phone number success saved")
+    @ApiResponse(responseCode = "404", description = "User not found")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    public ResponseEntity<PhoneNumberDTO> addToPhoneNumber(@RequestBody PhoneNumberDTO phoneNumberDTO,
+                                                           @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(userService.addToPhoneNumber(token, phoneNumberDTO));
+    }
+
+    @PutMapping("/phoneNumber")
+    @Operation(summary = "User phone number updated", description = "User phone number updated")
+    @ApiResponse(responseCode = "200", description = "Phone number success updated")
+    @ApiResponse(responseCode = "404", description = "User not found")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    public ResponseEntity<PhoneNumberDTO> updatePhoneNumber(@RequestBody PhoneNumberDTO phoneNumberDTO,
+                                                            @RequestParam("id") Long id,
+                                                            @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(userService.updatePhoneNumber(id, phoneNumberDTO, token));
+    }
+}
